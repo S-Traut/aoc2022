6a5075f8eff4c242ce1e36bc142e646dea09bbf5
s-traut <stephane.traut@gmail.com>
2022-12-01 20:29:44 +0100

    a bit of clean up for day 1

--- days/1.c
+++ days/1.c
@@ -1,31 +1,21 @@
+/*
+Advent Of Code 2022 - Stéphane Traut
+This version is pretty rough and not scalable, and can be optimized / clean up
+
+It basically just loop through each characters of the input string, after each line return it
+increase the current elf_value, if there is 2 line return it take the elf value and check for its
+order in the TOP 3.
+*/
+
 #include <lib.h>
-
-int get_elf_count(aoc_file_t input)
-{
-  int total = 0;
-  for (size_t i = 0; i < input.size; ++i)
-  {
-
-    char current_char = input.data[i];
-    char next_char = input.data[i + 1];
-
-    if (next_char == '\0' || '\n' == current_char && '\n' == next_char)
-      total++;
-  }
-
-  return total;
-}
 
 int main(void)
 {
   aoc_file_t input = get_data("./data/d1.txt");
-  u32 elf_count = get_elf_count(input);
-  int elf_data[elf_count];
-  u32 elf_index = 0;
   char BUFFER[10] = {0};
   u32 buffer_index = 0;
   int elf_value = 0;
-  int max = 0;
+  int first, second, third;
 
   for (size_t i = 0; i < input.size; ++i)
   {
@@ -35,12 +25,27 @@ int main(void)
     if (next_char == '\0' || '\n' == current_char && '\n' == next_char)
     {
       elf_value += atoi(BUFFER);
-      elf_data[elf_index] = elf_value;
+
+      // (TODO) Unscalable might be better to make it in a better way..
+      if (elf_value > first)
+      {
+        third = second;
+        second = first;
+        first = elf_value;
+      }
+      else if (elf_value > second)
+      {
+        third = second;
+        second = elf_value;
+      }
+      else if (elf_value > third)
+      {
+        third = elf_value;
+      }
 
       elf_value = 0;
       aoc_clear_buffer(BUFFER, 10);
       buffer_index = 0;
-      elf_index++;
       continue;
     }
 
@@ -54,27 +59,6 @@ int main(void)
     {
       BUFFER[buffer_index] = current_char;
       buffer_index++;
-    }
-  }
-
-  int first, second, third;
-  for (size_t i = 0; i < elf_count; i++)
-  {
-
-    if (elf_data[i] > first)
-    {
-      third = second;
-      second = first;
-      first = elf_data[i];
-    }
-    else if (elf_data[i] > second)
-    {
-      third = second;
-      second = elf_data[i];
-    }
-    else if (elf_data[i] > third)
-    {
-      third = elf_data[i];
     }
   }
 
