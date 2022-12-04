1e5db42ec1ad3a832b785041a5111c1607397489
s-traut <stephane.traut@gmail.com>
2022-12-04 16:28:57 +0100

    Cleanup day 3

--- days/3.c
+++ days/3.c
@@ -2,59 +2,63 @@
 Advent Of Code 2022 - Stéphane Traut
 DAY 3
 
-This solution is pretty bad / messy, and need to be rewritten.
+For this problem I have created a struct ruckstack_t that allow me to simply store
+a char* that is the data, and it's size.
+
+Because of C I had to create few functions to make the code more cleaner and explicit.
+
+For the first problem it's pretty easy and straight forward to get an answer, simply loop from 0 to
+size / 2 of a rucksack, and iterate another time on the second half of the rucksack to check if there
+is the same occurence of a char. If yes, return the priority of it.
+
+For the second problem, it was a bit more annoying because I had to store the 3 rucksack data in the heap to
+perform some check more easily with them. Every 3 rucksacks I give a group to a function that simple loop through
+the first one, and check if there is a char occurence in the 2 others.
+
+The 2 algorithms can be surely optimized, but for now, I prefer using these 2 simple solutions.
 */
 
 #include <lib.h>
 #include <string.h>
 #include <ctype.h>
 
-u32 get_rucksack_priorities(char *rucksack)
+bool rucksack_contain(rucksack_t rucksack, char item)
 {
-  u64 priority = 0;
-  u64 rucksack_size = strlen(rucksack);
-  u64 compartment_size = rucksack_size / 2;
-
-  for (u64 i = 0; i < compartment_size; ++i)
+  for (u32 i = 0; i < rucksack.size; i++)
   {
-    for (u64 j = 0; j < compartment_size; ++j)
+    if (rucksack.data[i] == item)
+      return AOC_TRUE;
+  }
+  return AOC_FALSE;
+}
+
+u32 get_rucksack_item_priority(char item)
+{
+  if (isupper(item))
+    return item - 38;
+
+  return item - 96;
+}
+
+u32 get_rucksack_priorities(rucksack_t rs)
+{
+  for (u64 i = 0; i < rs.compartment_size; ++i)
+  {
+    for (u64 j = 0; j < rs.compartment_size; ++j)
     {
-      if (rucksack[i] == rucksack[compartment_size + j])
-      {
-        char shared_item = rucksack[i];
-        if (isupper(shared_item))
-          return (shared_item - 65) + 27;
-        else
-          return (shared_item - 97) + 1;
-      }
+      if (rs.data[i] == rs.data[rs.compartment_size + j])
+        return get_rucksack_item_priority(rs.data[i]);
     }
   }
 }
 
-u32 find_badge(char *old, char *old2, char *current)
+u32 find_badge(const rucksack_t group[])
 {
-  u64 old_length = strlen(old);
-  u64 old2_length = strlen(old2);
-  u64 cur_length = strlen(current);
-  for (u64 i = 0; i < old_length; i++)
+  for (u32 i = 0; i < group[0].size; i++)
   {
-    for (u64 j = 0; j < old2_length; ++j)
-    {
-      if (old[i] == old2[j])
-      {
-        char shared_item = old[i];
-        for (u64 k = 0; k < cur_length; ++k)
-        {
-          if (current[k] == shared_item)
-          {
-            if (isupper(shared_item))
-              return (shared_item - 65) + 27;
-            else
-              return (shared_item - 97) + 1;
-          }
-        }
-      }
-    }
+    char current_item = group[0].data[i];
+    if (rucksack_contain(group[1], current_item) && rucksack_contain(group[2], current_item))
+      return get_rucksack_item_priority(current_item);
   }
 }
 
@@ -65,13 +69,10 @@ int main(void)
   size_t len = 0;
   size_t read;
 
-  u32 sum = 0;
+  u32 priority_sum = 0;
   u32 badge_sum = 0;
-  char badge = '\0';
-  u32 line_number = 0;
-
-  char *old_line = "";
-  char *old_line_2 = "";
+  u32 i = 0;
+  rucksack_t group[3];
 
   fp = fopen("./data/d3.txt", "r");
   if (fp == NULL)
@@ -80,33 +81,27 @@ int main(void)
   // Reading the file for each line:
   while ((read = getline(&line, &len, fp)) != -1)
   {
-    sum += get_rucksack_priorities(line);
+    u64 rucksack_size = strlen(line);
+    char *data = malloc(sizeof(char) * rucksack_size);
+    strcpy(data, line);
+    rucksack_t rucksack = {
+        .data = data,
+        .size = rucksack_size,
+        .compartment_size = rucksack_size / 2,
+    };
+    group[i % 3] = rucksack;
+    priority_sum += get_rucksack_priorities(rucksack);
 
-    if (line_number == 0)
+    if ((i + 1) % 3 == 0)
     {
-      old_line = malloc(sizeof(char) * len);
-      strcpy(old_line, line);
+      badge_sum += find_badge(group);
+      for (u8 i = 0; i < 3; ++i)
+        free(group[i].data);
     }
-
-    if (line_number == 1)
-    {
-      old_line_2 = malloc(sizeof(char) * len);
-      strcpy(old_line_2, line);
-    }
-
-    if (line_number == 2)
-    {
-      badge = find_badge(old_line, old_line_2, line);
-      badge_sum += badge;
-      line_number = 0;
-      free(old_line);
-      free(old_line_2);
-      continue;
-    }
-    line_number++;
+    i++;
   }
 
-  printf("Sum of all priorities: %d\n", sum);
+  printf("Sum of all priorities: %d\n", priority_sum);
   printf("Sum of badges: %d\n", badge_sum);
 
   fclose(fp);
--- include/lib.h
+++ include/lib.h
@@ -6,6 +6,10 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long u64;
+typedef unsigned char bool;
+
+#define AOC_TRUE 1
+#define AOC_FALSE 0
 
 typedef struct interval_t
 {
@@ -13,5 +17,12 @@ typedef struct interval_t
   u64 end;
   u64 length;
 } interval_t;
+
+typedef struct rucksack_t
+{
+  char *data;
+  u64 size;
+  u64 compartment_size;
+} rucksack_t;
 
 u8 aoc_interval_contains(interval_t inverval, u64 number);
